52c26182a4b22c9e84487e4a8cd9c1d38f37f5ab
Qodirbek <qodirbekpubg13@gmail.com>
2025-02-27 17:04:40 +0500

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,18 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Earnmoney</title>
+      </Head>
       <h1>Welcome to Earnmoney</h1>
-      <p>Start earning today!</p>
+      <p>Click the button below to pay with crypto:</p>
+      <a href="https://nowpayments.io/payment/?iid=5731964294" target="_blank" rel="noreferrer noopener">
+        <img src="https://nowpayments.io/images/embeds/payment-button-white.svg" 
+             alt="Cryptocurrency & Bitcoin payment button by NOWPayments"
+             style={{ width: "200px", cursor: "pointer" }}/>
+      </a>
     </div>
   );
 }
